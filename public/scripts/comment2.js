4b23fd4fa43e833002fb3b968c7f3f1980db01f6
Jose Lopez <gollo_autumn@live.com>
2015-05-08 21:14:41 -0430

    add public folder

---
+++ public/scripts/comment2.js
@@ -0,0 +1,41 @@
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+    	var commentNodes = this.props.data.map(function (comment) {
+	      return (
+	        <Comment author={comment.author}>
+	          {comment.text}
+	        </Comment>
+	      );
+	    });
+      <div className="commentList">
+         {commentNodes}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+	handleSubmit: function(e) {
+	    e.preventDefault();
+	    var author = React.findDOMNode(this.refs.author).value.trim();
+	    var text = React.findDOMNode(this.refs.text).value.trim();
+	    if (!text || !author) {
+	      return;
+	    }
+	    this.props.onCommentSubmit({author: author, text: text});
+	    React.findDOMNode(this.refs.author).value = '';
+	    React.findDOMNode(this.refs.text).value = '';
+	    return;
+  	},
+	render: function() {
+	    return (
+	      <form className="commentForm" onSubmit = { this.handleSubmit } >
+		        <input type="text" placeholder="Your name" ref="author" />
+		        <input type="text" placeholder="Say something..." ref="text" />
+		        <input type="submit" value="Post" />
+	      </form>
+		);
+  	}
+});
